251d507ab1a5204b3da2101e28938ae1e429e479
jcdevivo <18503260+jcdevivo@users.noreply.github.com>
2018-07-12 15:07:03 -0600

    update

--- R/GetTaxaFromIRMA.R
+++ R/GetTaxaFromIRMA.R
@@ -28,3 +28,12 @@ taxaKingdom<-function(x) {
     KingdomTaxa <- NPSpeciesJustSpecies[NPSpeciesJustSpecies$Kingdom==x,]
     return(KingdomTaxa)
 }
+
+listTaxaCategories<-function(y) {
+  unique(NPSpeciesJustSpecies$CategoryName)
+}
+
+taxaCategories<-function(z) {
+  CategoryTaxa <- NPSpeciesJustSpecies[NPSpeciesJustSpecies$CategoryName==z,]
+  return(CategoryTaxa)
+}
