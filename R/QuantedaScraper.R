53cf3783a873f80c0e3d72f661b5d82afc7fcfd2
jcdevivo <jcdevivo@gmail.com>
2018-07-17 09:36:18 -0600

    morning commit

--- R/QuantedaScraper.R
+++ R/QuantedaScraper.R
@@ -14,11 +14,36 @@ package.check <- lapply(packages, FUN = function(x) {
 
 })
 
+start_time <- Sys.time()
+
+# Load most recent version of NPSpecies Taxonomy table and create Species dictionary file, This step only needs to be done once per session.
+
 load("data/NPSpeciesJustSpecies.rda")
 
-doclocation <- file.path("D:/texts")
+dicSpecies<-dictionary(list(birds=c(taxaCategories("Bird")),
+                            fish=c(taxaCategories("Fish")),
+                            insects=c(taxaCategories("Insect")),
+                            chromista=c(taxaCategories("Chromista")),
+                            bacteria=c(taxaCategories("Bacteria")),
+                            othernonverts=c(taxaCategories("Other Non-vertebrates")),
+                            NonvascularPlants=c(taxaCategories("Non-vascular Plant")),
+                            fungi=c(taxaCategories("Fungi")),
+                            protozoans=c(taxaCategories("Protozoa")),
+                            VascularPlantslis=c(taxaCategories("Vascular Plant")),
+                            slugsandsnails=c(taxaCategories("Slug/Snail")),
+                            amphibians=c(taxaCategories("Amphibian")),
+                            reptiles=c(taxaCategories("Reptile")),
+                            mammals=c(taxaCategories("Mammal")),
+                            CrabsLobsersShrimp=c(taxaCategories("Crab/Lobster/Shrimp")),
+                            SpiderScorpion=c(taxaCategories("Spider/Scorpion")),
+                            Archaea=c(taxaCategories("Archaea"))
+), tolower=FALSE)
 
-taxatokens<-phrase(dicSpecies)
+
+# Read in pdf files and check for species contained in the dictionary. Test files are in the data directory with the package.
+
+# doclocation <- file.path("D:/texts")
+doclocation <- file.path("data")
 
 docs<-readtext(paste0(doclocation, "/*.pdf"),
               docvarsfrom = "filenames",
@@ -26,7 +51,13 @@ docs<-readtext(paste0(doclocation, "/*.pdf"),
                )
 
 docscorpus<-corpus(docs)
+hits<-as.data.frame(kwic(docscorpus,phrase(dicSpecies),valuetype="fixed", include_docvars=TRUE))
 
-docsdfm<-dfm(docscorpus, tolower=FALSE, select=dicSpecies, valuetype = "fixed")
+# Write results to CSV files
+results<-distinct(data.frame(IRMAReferenceNumber=gsub(".pdf","",hits$docname),SciName=hits$keyword))
 
-hits<-dfm_lookup(mydocs,levels=1:2,dictionary=TaxonomicDictionary, valuetype="fixed")
+refs<-unique(results$IRMAReferenceNumber)
+for (i in 1:length(refs)){
+  write.csv(subset(results, IRMAReferenceNumber==refs[i]), paste("results/",refs[i],"_NPSpeciesSpecies.csv", sep=""), row.names=F)
+}
+print(Sys.time()-start_time)
